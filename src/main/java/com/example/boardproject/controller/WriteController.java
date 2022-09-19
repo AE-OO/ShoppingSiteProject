aef0871e3b3262150df4724e7317aaf5cab9b896
oh <wcaqihgn@gmail.com>
2022-09-19 22:21:10 +0900

    shopping site update v1.6

--- src/main/java/com/example/boardproject/controller/WriteController.java
+++ src/main/java/com/example/boardproject/controller/WriteController.java
@@ -15,8 +15,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -68,10 +70,40 @@ public class WriteController {
      */
     @GetMapping("/test")
     public String test(Model model) {
-        Optional<Board> result1 = writeRepository.findById(217);
+        Optional<Board> result1 = writeRepository.findById(328);
         Board result = result1.get();
         model.addAttribute("img", result);
         return "layout/defaultForm";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+
+
+        return "/layout/test";
+    }
+
+    @PostMapping("/test2")
+    public String test3(@RequestParam(name = "pImageFiles") List<MultipartFile> result) throws IOException {
+
+        List<UploadFile> uploadFiles = fileService.storeFiles(result);
+        uploadFiles.stream().forEach(i -> {
+
+        });
+
+
+        for (MultipartFile multipartFile : result) {
+            UploadFile uploadFile = fileService.storeFile(multipartFile);
+            String storeFileName = uploadFile.getStoreFileName();
+            String uploadFileName = uploadFile.getUploadFileName();
+            System.out.println("storeFileName = " + storeFileName);
+            System.out.println("uploadFileName = " + uploadFileName);
+
+            if (result.isEmpty()) {
+
+            }
+        }
+        return "/layout/test";
     }
 }
 
